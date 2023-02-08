4a35e553b973c85ce13c915f72ff88b81680d5a7
Matt <matthewdavid99@comcast.net>
2023-02-08 16:32:23 -0500

    Successfully added firstName and lastName and got correct output

--- Authenticate-Me/backend/db/models/user.js
+++ Authenticate-Me/backend/db/models/user.js
@@ -9,8 +9,8 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
 
     toSafeObject() {
-      const { id, username, email } = this; // context will be the User instance
-      return { id, username, email };
+      const { id, username, email, firstName, lastName } = this; // context will be the User instance
+      return { id, username, email, firstName, lastName };
     }
 
     validatePassword(password) {
@@ -36,12 +36,14 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
 
-    static async signup({ username, email, password }) {
+    static async signup({ username, email, password, firstName, lastName }) {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
         username,
         email,
-        hashedPassword
+        hashedPassword,
+        firstName,
+        lastName
       });
       return await User.scope('currentUser').findByPk(user.id);
     }
--- Authenticate-Me/backend/routes/api/users.js
+++ Authenticate-Me/backend/routes/api/users.js
@@ -51,4 +51,6 @@ router.post(
 
 
 
+
+
 module.exports = router;
