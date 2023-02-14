2a72d5d9c927e068a8c394ecdce0ac0677b70de8
Matt <matthewdavid99@comcast.net>
2023-02-14 18:44:40 -0500

    redo

--- backend/db/migrations/20230212002619-create-spot.js
+++ backend/db/migrations/20230212002619-create-spot.js
@@ -3,6 +3,7 @@
 let options = {}
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;  // define your schema in options object
+  // options.tableName = 'Spots'
 }
 
 module.exports = {
