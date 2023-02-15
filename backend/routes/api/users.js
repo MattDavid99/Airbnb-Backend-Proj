3afebd68ae585cc9d687ee3cc0b8f57ed6714a36
Matt <matthewdavid99@comcast.net>
2023-02-15 17:50:06 -0500

    user.js sign-up route json/error-handleing done

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -68,13 +68,24 @@ router.post(
 
         } catch (error) {
             // return res.json(error)
-            const dynamicError = error.errors.map((ele) => {
+            error.errors.map((ele) => {
+
                 if (ele.path == "username") {
                     return res.json({
                         message: "User already exists",
                         statusCode: 403,
                         errors: {
-                            username: `User with ${ele.value} already exists`
+                            username: `User with that username already exists`
+                        }
+                    })
+                }
+
+                if (ele.path == "email") {
+                    return res.json({
+                        message: "User already exists",
+                        statusCode: 403,
+                        errors: {
+                            email: `User with that email already exists`
                         }
                     })
                 }
@@ -87,7 +98,53 @@ router.post(
 
 
 
+//_____________________________________________________________⬇️⬇️ This is the error obj we're working with
+/*
 
+{
+    "name": "SequelizeUniqueConstraintError",
+    "errors": [
+        {
+            "message": "email must be unique",
+            "type": "unique violation",
+            "path": "email",
+            "value": "jn.smahahahahahaith@gmail.com",
+            "origin": "DB",
+            "instance": {
+                "id": null,
+                "username": "hahahahckDav",
+                "email": "jn.smahahahahahaith@gmail.com",
+                "hashedPassword": "$2a$10$fw.1k7DYdvc1J2nvR40XKuk.BfsyZC/awxXNZorcf3Dvqc4jGiK9O",
+                "firstName": "hah",
+                "lastName": "hahahaha",
+                "updatedAt": "2023-02-15T22:44:50.677Z",
+                "createdAt": "2023-02-15T22:44:50.677Z"
+            },
+            "validatorKey": "not_unique",
+            "validatorName": null,
+            "validatorArgs": []
+        }
+    ],
+    "parent": {
+        "errno": 19,
+        "code": "SQLITE_CONSTRAINT",
+        "sql": "INSERT INTO `Users` (`id`,`firstName`,`lastName`,`username`,`email`,`hashedPassword`,`createdAt`,`updatedAt`) VALUES (NULL,$1,$2,$3,$4,$5,$6,$7);"
+    },
+    "original": {
+        "errno": 19,
+        "code": "SQLITE_CONSTRAINT",
+        "sql": "INSERT INTO `Users` (`id`,`firstName`,`lastName`,`username`,`email`,`hashedPassword`,`createdAt`,`updatedAt`) VALUES (NULL,$1,$2,$3,$4,$5,$6,$7);"
+    },
+    "fields": [
+        "email"
+    ],
+    "sql": "INSERT INTO `Users` (`id`,`firstName`,`lastName`,`username`,`email`,`hashedPassword`,`createdAt`,`updatedAt`) VALUES (NULL,$1,$2,$3,$4,$5,$6,$7);"
+}
+
+
+
+
+*/
 
 
 
