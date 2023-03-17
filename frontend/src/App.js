56dc1af9463e674fd59738629a91bbc3e2dcc415
Matt <matthewdavid99@comcast.net>
2023-03-17 16:59:26 -0400

    authME phase-1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,33 @@
+// frontend/src/App.js
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import * as sessionActions from "./store/session";
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  // return (
+  // <Switch>
+  {/* <Route path="/login"> */ }
+  {/* <LoginFormPage /> */ }
+  {/* </Route> */ }
+  {/* </Switch> */ }
+  // );
+  return isLoaded && (
+    <>
+      <div>hi</div>
+      <Switch>
+        <Route path="/login">
+          <LoginFormPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import './index.css';
 import configureStore from './store';
-
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -21,6 +21,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 
