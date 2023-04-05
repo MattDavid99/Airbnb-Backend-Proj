b7fc00593043a7e5d7655945da00a638d493c8d2
Matt <matthewdavid99@comcast.net>
2023-04-05 15:12:02 -0400

    added styles to SpotIdPage component

--- frontend/src/components/SpotIdPage/index.js
+++ frontend/src/components/SpotIdPage/index.js
@@ -10,30 +10,13 @@ function SpotIdPage() {
   const dispatch = useDispatch()
   const { id } = useParams()
 
-  // const specificSpot = useSelector((state) => (state.session.spots))
   const specificSpot = useSelector((state) => state.session.spots.find(spot => spot.id === +id));
   console.log(specificSpot);
 
-  // const finder = specificSpot.find((spot) => spot.id + id)
-  // console.log(finder);
-
-  // const spot = useSelector((state) => state)
-  // console.log(spot);
-
-
-
-  // const specificSpot = useSelector((state) => {
-  //   const spot = state.session.spots.find((spot) => spot.id === +id);
-  //   if (spot) {
-  //     return { ...spot, SpotImages: state.session.SpotImages };
-  //   }
-  //   return null;
-  // });
   useEffect(() => {
     dispatch(getSpotId(id))
   }, [dispatch, id])
 
-  // console.log(specificSpot.SpotImages);
 
 
   return (
@@ -45,14 +28,25 @@ function SpotIdPage() {
         </div>
 
         <div className='spot-id-page-image-div'>
-          {specificSpot.SpotImages && specificSpot.SpotImages.map((i) => (
+          {specificSpot.SpotImages && specificSpot.SpotImages.map((i, index) => (
 
-            <img src={i.url} alt="#" className='spot-id-page-image' key={i.id} />
+            <img src={i.url} alt="#" className={index === 0 ? 'spot-id-page-first-image' : 'spot-id-page-image'} key={i.id} />
           ))}
         </div>
 
+        {specificSpot.Owner && (
+          <div className='spot-id-page-host-div'>
+            <h3 className='spot-id-page-host-h3'>
+              Hosted by: {specificSpot.Owner.firstName === null ? 'Matthew David' : specificSpot.Owner.firstName}
+            </h3>
+
+            <h4 className='spot-id-page-host-h3'>Description: {specificSpot.description}</h4>
+          </div>
+        )}
+
       </div>
     </div>
+
 
   )
 }
