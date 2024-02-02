67c90cc4e7ee25b099ca108aa58f475c47ed0853
Matthew David <102698225+MattDavid99@users.noreply.github.com>
2024-02-02 14:02:59 -0500

    Update index.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,34 +9,13 @@ const spotImagesRouter = require('./spot-images.js')
 const reviewImagesRouter = require('./review-images.js')
 const { restoreUser } = require("../../utils/auth.js");
 
-// ---------------------------------------------------------⬇️
-// const router = require('express').Router();
-// const sessionRouter = require('./session.js');
-// const usersRouter = require('./users.js');
-
-// const { setTokenCookie } = require('../../utils/auth.js');
-// const { User } = require('../../db/models');
-// // GET /api/restore-user
-// const { restoreUser } = require('../../utils/auth.js');
-// const { requireAuth } = require('../../utils/auth.js');
-
-// Connect restoreUser middleware to the API router
-// If current user session is valid, set req.user to the user in the database
-// If current user session is not valid, set req.user to null
 router.use(restoreUser);
-
 router.use('/session', sessionRouter);
-
 router.use('/users', usersRouter);
-
 router.use('/spots', spotsRouter);
-
 router.use('/reviews', reviewsRouter);
-
 router.use('/bookings', bookingsRouter);
-
 router.use('/spot-images', spotImagesRouter);
-
 router.use('/review-images', reviewImagesRouter);
 
 router.post('/test', (req, res) => {
